130777764566b9be89f87a002de18c074a8ce92a
lununes <luciana.mn@gmail.com>
2021-01-17 04:54:49 -0300

    Inserção do menu de tópicos

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,19 @@
 jQuery(document).ready(function($) {
 
-    // Header fixed and Back to top button
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 90) {
-        $('#nav-menu-container').addClass('d-block');
-        $('#nav-menu-container').fadeIn('50');
-        $('#nav-menu-container').removeClass('d-none');
-      } else {
-        $('#nav-menu-container').removeClass('d-block');
-        $('#nav-menu-container').fadeOut('50', function() {
-          $('#nav-menu-container').addClass('d-none');
-        });
-        
-      }
-    });
+    // No desktop, quando scrolla, exibe o menu de tópicos a partir de certa altura
+    if(window.outerWidth > 768){
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 90) {
+          $('#nav-menu-container').addClass('d-block');
+          $('#nav-menu-container').fadeIn('50');
+          $('#nav-menu-container').removeClass('d-lg-none');
+        } else {
+          $('#nav-menu-container').removeClass('d-block');
+          $('#nav-menu-container').fadeOut('50', function() {
+            $('#nav-menu-container').addClass('d-lg-none');
+          });
+          
+        }
+      });
+    }
 });
