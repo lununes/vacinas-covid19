90d492164ac5b4a13a490cda720a3874acc09770
lununes <luciana.mn@gmail.com>
2021-01-25 00:29:56 -0300

    Ajustes Módulo 4 > aula 1

--- js/custom.js
+++ js/custom.js
@@ -16,4 +16,28 @@ jQuery(document).ready(function($) {
         }
       });
     }
+
+    // Configura o Slider com cards
+    $('.carousel-cards').carousel({
+			interval: 10000
+			})
+
+			$('.carousel.carousel-cards .carousel-item').each(function(){
+				var minPerSlide = 3;
+				var next = $(this).next();
+				if (!next.length) {
+				next = $(this).siblings(':first');
+				}
+				next.children(':first-child').clone().appendTo($(this));
+				
+				for (var i=0;i<minPerSlide;i++) {
+					next=next.next();
+					if (!next.length) {
+						next = $(this).siblings(':first');
+					}
+					
+					next.children(':first-child').clone().appendTo($(this));
+				}
+			});
 });
+
